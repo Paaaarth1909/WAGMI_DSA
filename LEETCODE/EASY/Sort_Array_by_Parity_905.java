47bcb98d7c233b33316d12d5f36ccf34af22740e
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-16 23:20:57 +0530

    COMMIT

--- LEETCODE/EASY/Sort_Array_by_Parity_905.java
+++ LEETCODE/EASY/Sort_Array_by_Parity_905.java
@@ -20,3 +20,18 @@ Constraints:
 1 <= nums.length <= 5000
 0 <= nums[i] <= 5000
 */
+class Solution {
+    public int[] sortArrayByParity(int[] A) {
+        int lo = 0, hi = A.length - 1;
+        while (lo < hi) {
+            if (A[lo] % 2 > A[hi] % 2) {
+                int tmp = A[hi];
+                A[hi] = A[lo];
+                A[lo] = tmp;
+            }
+            if (A[lo] % 2 == 0) lo++;
+            if (A[hi] % 2 == 1) hi--;
+        }
+        return A;
+    }
+}
