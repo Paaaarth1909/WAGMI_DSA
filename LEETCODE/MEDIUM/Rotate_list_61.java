b4d191be0a98366d23126f5b8b40965d8d295769
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-18 22:40:48 +0530

    COMMIT

--- LEETCODE/MEDIUM/Rotate_list_61.java
+++ LEETCODE/MEDIUM/Rotate_list_61.java
@@ -20,3 +20,28 @@ The number of nodes in the list is in the range [0, 500].
 -100 <= Node.val <= 100
 0 <= k <= 2 * 109
 */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) return head;
+        
+        ListNode curr = head;
+        int length = 1;
+        while (curr.next != null) {
+            curr = curr.next;
+            length++;
+        }
+        
+        curr.next = head;
+        k = k % length;
+        int steps = length - k;
+        
+        while (steps-- > 0) {
+            curr = curr.next;
+        }
+        
+        head = curr.next;
+        curr.next = null;
+        
+        return head;
+    }
+}
