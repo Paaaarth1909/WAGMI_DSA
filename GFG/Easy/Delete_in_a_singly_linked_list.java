eda3e58612fabef890aa4f951f4ab2dee3e3e27b
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-19 20:25:01 +0530

    COMMIT

--- GFG/Easy/Delete_in_a_singly_linked_list.java
+++ GFG/Easy/Delete_in_a_singly_linked_list.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* You are given the head of a singly linked list and an integer x. Delete the xth node (1-based indexing) from the singly linked list.
 
 Examples:
@@ -17,3 +19,38 @@ Constraints:
 1 ≤ x ≤ size of linked list
 
 */
+
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+
+class Solution {
+    Node deleteNode(Node head, int x) {
+        if (head == null) return null;
+        
+        // If x == 1, delete the head node
+        if (x == 1) {
+            return head.next;
+        }
+        
+        Node curr = head;
+        for (int i = 1; i < x - 1 && curr != null; i++) {
+            curr = curr.next;
+        }
+        
+        // curr is now (x-1)th node
+        if (curr != null && curr.next != null) {
+            curr.next = curr.next.next;
+        }
+        
+        return head;
+    }
+}
