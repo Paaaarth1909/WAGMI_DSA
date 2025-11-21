281e7812e435909ef24a175cc0d67d1f308c1fe9
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-21 16:08:35 +0530

    COMMIT

--- GFG/Medium/Implement_stack_using_array.java
+++ GFG/Medium/Implement_stack_using_array.java
@@ -47,3 +47,39 @@ Constraints:
 1 ≤ q ≤ 103
 0 ≤ x ≤ 105
 */
+class myStack {
+    int[] arr;
+    int top;
+    int capacity;
+
+    public myStack(int n) {
+        arr = new int[n];
+        top = -1;
+        capacity = n;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void push(int x) {
+        if (!isFull()) {
+            arr[++top] = x;
+        }
+    }
+
+    public void pop() {
+        if (!isEmpty()) {
+            top--;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) return -1;
+        return arr[top];
+    }
+}
