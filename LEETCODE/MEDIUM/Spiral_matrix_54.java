2fc669d153e52a9d5ce91058971ea99891d71806
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-23 19:06:15 +0530

    COMMIT

--- LEETCODE/MEDIUM/Spiral_matrix_54.java
+++ LEETCODE/MEDIUM/Spiral_matrix_54.java
@@ -21,3 +21,30 @@ n == matrix[i].length
 1 <= m, n <= 10
 -100 <= matrix[i][j] <= 100
 */
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) res.add(matrix[top][i]);
+            top++;
+
+            for (int i = top; i <= bottom; i++) res.add(matrix[i][right]);
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) res.add(matrix[bottom][i]);
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) res.add(matrix[i][left]);
+                left++;
+            }
+        }
+
+        return res;
+    }
+}
