7ac1f87727203961f585d8aeaa6f38f1093390f9
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-27 19:51:29 +0530

    COMMIT

--- GFG/Easy/Stack_using_linked_list.java
+++ GFG/Easy/Stack_using_linked_list.java
@@ -40,3 +40,52 @@ Constraints:
 0 ≤ x ≤ 105
 
 */
+// Node class
+/* class Node {
+    int data;
+    Node next;
+
+    Node(int new_data) {
+        data = new_data;
+        next = null;
+    }
+} */
+
+// Stack class
+class myStack {
+
+    Node top;
+    int size;
+
+    public myStack() {
+        top = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return top == null;
+    }
+
+    public void push(int x) {
+        Node temp = new Node(x);
+        temp.next = top;
+        top = temp;
+        size++;
+    }
+
+    public void pop() {
+        if (!isEmpty()) {
+            top = top.next;
+            size--;
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) return -1;
+        return top.data;
+    }
+
+    public int size() {
+        return size;
+    }
+}
