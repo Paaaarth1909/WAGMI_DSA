48ac419603ebef0d2ce1ae2877c25970219fe320
Parthsaarthie sharma <toshit113@gmail.com>
2025-11-30 11:43:43 +0530

    COMMIT

--- GFG/Easy/Move_all_zeroes_to_end.java
+++ GFG/Easy/Move_all_zeroes_to_end.java
@@ -17,3 +17,31 @@ Constraints:
 1 ≤ arr.size() ≤ 105
 0 ≤ arr[i] ≤ 105
 */
+class Solution {
+    static void pushZerosToEnd(int[] arr) {
+        int n = arr.length ; 
+        int [] temp = new int[n];
+        int j=0;
+        for ( int i=0 ; i<n ; i++  ) {
+            if ( arr[i]!=0)
+            temp[j++]=arr[i];
+            
+        }
+        while ( j<n)
+        temp[j++]=0;
+        for ( int i=0 ; i<n ; i++ ) {
+            arr[i]=temp[i];
+        }
+    }
+        public static void main (String [] args ){
+            int [] arr={1,2,0,4,3,0,5,0};
+            pushZerosToEnd(arr);
+            
+            for ( int num :arr ) {
+                System.out.print(num + "");
+            }
+            
+        }
+}
+        
+    
