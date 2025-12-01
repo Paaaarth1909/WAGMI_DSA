50070956aa3542dec60e2e7ade3867b3248911f0
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-01 23:34:34 +0530

    COMMIT

--- LEETCODE/EASY/Pascal's_traingle_118.java
+++ LEETCODE/EASY/Pascal's_traingle_118.java
@@ -20,6 +20,10 @@ Constraints:
 1 <= numRows <= 30
  
 */
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
