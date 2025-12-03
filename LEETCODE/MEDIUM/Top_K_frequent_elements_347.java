14dae3a5ecf43a37b99c1284b945bad727da55c0
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-03 22:50:31 +0530

    COMMIT

--- LEETCODE/MEDIUM/Top_K_frequent_elements_347.java
+++ LEETCODE/MEDIUM/Top_K_frequent_elements_347.java
@@ -29,3 +29,21 @@ Constraints:
 k is in the range [1, the number of unique elements in the array].
 It is guaranteed that the answer is unique.
 */
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<>();
+        for (int n : nums) count.put(n, count.getOrDefault(n, 0) + 1);
+
+        PriorityQueue<Integer> heap = new PriorityQueue<>((a, b) -> count.get(a) - count.get(b));
+        
+        for (int n : count.keySet()) {
+            heap.add(n);
+            if (heap.size() > k) heap.poll();
+        }
+        
+        int[] res = new int[k];
+        int i = 0;
+        while (!heap.isEmpty()) res[i++] = heap.poll();
+        return res;
+    }
+}
