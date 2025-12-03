514bf5416e3ce3ac31563a2356d279b22175ffd0
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-03 22:56:28 +0530

    COMMIT

--- GFG/Easy/Binary_search.java
+++ GFG/Easy/Binary_search.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a sorted array arr[] and an integer k, find the position(0-based indexing) at which k is present in the array using binary search. If k doesn't exist in arr[] return -1. 
 
 Note: If multiple occurrences are there, please return the smallest index.
