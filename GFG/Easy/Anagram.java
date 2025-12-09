5c0adc450e76632a4c94d638cde4c08874f50064
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-09 23:51:37 +0530

    COMMIT

--- GFG/Easy/Anagram.java
+++ GFG/Easy/Anagram.java
@@ -17,3 +17,21 @@ Constraints:
 s1, s2 consists of lowercase English letters.
 
 */
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+
+        int[] freq = new int[26];
+
+        for (char c : s1.toCharArray())
+            freq[c - 'a']++;
+
+        for (char c : s2.toCharArray())
+            freq[c - 'a']--;
+
+        for (int x : freq)
+            if (x != 0) return false;
+
+        return true;
+    }
+}
