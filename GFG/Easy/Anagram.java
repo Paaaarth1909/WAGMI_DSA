0d829a0bec50f90af99b3e65ec1e338b9086384a
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-09 23:51:46 +0530

    COMMIT

--- GFG/Easy/Anagram.java
+++ GFG/Easy/Anagram.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given two non-empty strings s1 and s2, consisting only of lowercase English letters, determine whether they are anagrams of each other or not.
 Two strings are considered anagrams if they contain the same characters with exactly the same frequencies, regardless of their order.
 
