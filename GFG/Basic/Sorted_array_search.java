36c72f38ac4097b94cc9dec9a6dd0d787c521e56
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-13 23:25:02 +0530

    COMMIT

--- GFG/Basic/Sorted_array_search.java
+++ GFG/Basic/Sorted_array_search.java
@@ -16,3 +16,18 @@ Constraints:
 1 ≤ arr[i] ≤ 106
 
  */
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) return true;
+            else if (arr[mid] < k) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return false;
+    }
+}
