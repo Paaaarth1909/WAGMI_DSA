0c96c4f1eddfbfa5f8b42db0e510ee2e4d373a55
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-13 21:38:12 +0530

    COMMIT

--- LEETCODE/MEDIUM/4Sum_18.java
+++ LEETCODE/MEDIUM/4Sum_18.java
@@ -23,3 +23,49 @@ Constraints:
 -109 <= nums[i] <= 109
 -109 <= target <= 109
 */
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        List<List<Integer>> res = new ArrayList<>();
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            long min1 = (long)nums[i] + nums[i+1] + nums[i+2] + nums[i+3];
+            if (min1 > target) break;
+
+            long max1 = (long)nums[i] + nums[n-1] + nums[n-2] + nums[n-3];
+            if (max1 < target) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                long min2 = (long)nums[i] + nums[j] + nums[j+1] + nums[j+2];
+                if (min2 > target) break;
+
+                long max2 = (long)nums[i] + nums[j] + nums[n-1] + nums[n-2];
+                if (max2 < target) continue;
+
+                int l = j + 1, r = n - 1;
+                long newTarget = (long)target - nums[i] - nums[j];
+
+                while (l < r) {
+                    long sum = nums[l] + nums[r];
+                    if (sum == newTarget) {
+                        res.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+                        l++;
+                        r--;
+                        while (l < r && nums[l] == nums[l - 1]) l++;
+                        while (l < r && nums[r] == nums[r + 1]) r--;
+                    } else if (sum < newTarget) {
+                        l++;
+                    } else {
+                        r--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+}
