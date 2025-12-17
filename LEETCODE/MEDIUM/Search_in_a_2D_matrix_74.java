f549f3b669c984a8c3cd9c04ccf3fdd31349b929
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-17 11:54:58 +0530

    COMMIT

--- LEETCODE/MEDIUM/Search_in_a_2D_matrix_74.java
+++ LEETCODE/MEDIUM/Search_in_a_2D_matrix_74.java
@@ -27,3 +27,42 @@ n == matrix[i].length
 1 <= m, n <= 100
 -104 <= matrix[i][j], target <= 104
 */
+class Solution {
+    int rows = 0;
+    int cols = 0;
+    int[][] m;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
+            return false;
+        }
+
+        m = matrix;
+        rows = matrix.length;
+        cols = matrix[0].length;
+
+        int start = 0;
+        int end = rows * cols - 1;
+
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (getValue(mid) == target) {
+                return true;
+            } else if (getValue(mid) < target) {
+                start = mid;
+            } else {
+                end = mid;
+            }
+        }
+
+        if (getValue(start) == target) {
+            return true;
+        }
+        if (getValue(end) == target) {
+            return true;
+        }
+        return false;
+    }
+ private int getValue(int index) {
+        return m[index / cols][index % cols];
+    }
+}
