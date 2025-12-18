c955b968bc557797fa62cb16720fc1beb71d1c47
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-18 08:07:40 +0530

    COMMIT

--- GFG/Hard/Max_circular_subarray_sum.java
+++ GFG/Hard/Max_circular_subarray_sum.java
@@ -16,3 +16,23 @@ Constraints:
 -104 ≤ arr[i] ≤ 104
 
 */
+int maxCircularSum(int* arr, int n) {
+    int total = 0;
+    int curMax = 0, maxSum = arr[0];
+    int curMin = 0, minSum = arr[0];
+
+    for (int i = 0; i < n; i++) {
+        total += arr[i];
+
+        curMax = (curMax + arr[i] > arr[i]) ? curMax + arr[i] : arr[i];
+        maxSum = (maxSum > curMax) ? maxSum : curMax;
+
+        curMin = (curMin + arr[i] < arr[i]) ? curMin + arr[i] : arr[i];
+        minSum = (minSum < curMin) ? minSum : curMin;
+    }
+
+    if (minSum == total)
+        return maxSum;
+
+    return (maxSum > (total - minSum)) ? maxSum : (total - minSum);
+}
