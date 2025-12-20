76d9ad35b6a6f9574d25d0b24f7db553fc344753
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-20 08:24:46 +0530

    COMMIT

--- GFG/Easy/Bus_conductor.java
+++ GFG/Easy/Bus_conductor.java
@@ -26,3 +26,17 @@ Constraints:
 1 ≤ n ≤ 105
 1 ≤ chairs[i], passengers[j] ≤ 104 
 */
+import java.util.*;
+
+class Solution {
+    public int findMoves(int[] chairs, int[] passengers) {
+        Arrays.sort(chairs);
+        Arrays.sort(passengers);
+
+        int moves = 0;
+        for (int i = 0; i < chairs.length; i++) {
+            moves += Math.abs(chairs[i] - passengers[i]);
+        }
+        return moves;
+    }
+}
