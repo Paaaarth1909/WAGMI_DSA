c0824d58659d8172ded187cfb05d8c8891d5f38e
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-21 12:01:58 +0530

    COMMIT

--- GFG/Easy/Search_insert_position_of_K_in_a_sorted_array.java
+++ GFG/Easy/Search_insert_position_of_K_in_a_sorted_array.java
@@ -17,3 +17,18 @@ Constraints:
 -103 ≤ k ≤ 103
 
 */
+class Solution {
+    public int searchInsertK(int arr[], int k) {
+        int low = 0, high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == k) return mid;
+            else if (arr[mid] < k) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low;
+    }
+}
