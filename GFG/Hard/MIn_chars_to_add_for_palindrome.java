7f5bf2deb5df37d0b4e5dbee7c3762e5e3843ba9
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-22 13:19:47 +0530

    COMMIT

--- GFG/Hard/MIn_chars_to_add_for_palindrome.java
+++ GFG/Hard/MIn_chars_to_add_for_palindrome.java
@@ -15,3 +15,28 @@ Constraints:
 s consists of lowercase english alphabets
 
 */
+class Solution {
+    public int minChar(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        String combined = s + "#" + rev;
+
+        int[] lps = new int[combined.length()];
+        int i = 1, len = 0;
+
+        while (i < combined.length()) {
+            if (combined.charAt(i) == combined.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) len = lps[len - 1];
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        return s.length() - lps[combined.length() - 1];
+    }
+}
