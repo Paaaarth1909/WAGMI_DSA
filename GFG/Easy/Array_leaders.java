bfb726f3602d35e92d94c68ff17b7594f799ca49
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-28 23:11:40 +0530

    COMMIT

--- GFG/Easy/Array_leaders.java
+++ GFG/Easy/Array_leaders.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* You are given an array arr of positive integers. Your task is to find all the leaders in the array. An element is considered a leader if it is greater than or equal to all elements to its right. The rightmost element is always a leader.
 
 Examples:
