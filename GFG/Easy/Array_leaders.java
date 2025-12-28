bedaddae2ab9ec791254ad20f9acdc6b7bc28e05
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-28 23:11:32 +0530

    COMMIT

--- GFG/Easy/Array_leaders.java
+++ GFG/Easy/Array_leaders.java
@@ -19,3 +19,23 @@ Constraints:
 0 <= arr[i] <= 106
 
 */
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        ArrayList<Integer> res = new ArrayList<>();
+        int maxSoFar = arr[arr.length - 1];
+        res.add(maxSoFar);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] >= maxSoFar) {
+                maxSoFar = arr[i];
+                res.add(arr[i]);
+            }
+        }
+
+        Collections.reverse(res);
+        return res;
+    }
+}
+
