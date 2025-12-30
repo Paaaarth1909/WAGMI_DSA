688e543a8cfc0aa9be3e0600899545c34d6beb91
Parthsaarthie sharma <toshit113@gmail.com>
2025-12-30 14:13:04 +0530

    COMMIT

--- GFG/Easy/Two_sum_Pair_with_given_sum.java
+++ GFG/Easy/Two_sum_Pair_with_given_sum.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given an array arr[] of integers and another integer target. Determine if there exist two distinct indices such that the sum of their elements is equal to the target.
 
 Examples:
