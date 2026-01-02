eb182dd8cfc031e3854e5cedf78df1b308756dc6
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-02 18:43:30 +0530

    COMMIT

--- GFG/Easy/Intersection_of_two_arrays_with_different_elements.java
+++ GFG/Easy/Intersection_of_two_arrays_with_different_elements.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given two integer arrays a[] and b[], you have to find the intersection of the two arrays. Intersection of two arrays is said to be elements that are common in both the arrays. The intersection should not have duplicate elements and the result should contain items in any order.
 
 Note: The driver code will sort the resulting array in increasing order before printing.
