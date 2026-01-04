c295fd258534c8c4d69f0dc659276b787e85031f
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-04 18:45:53 +0530

    COMMIT

--- GFG/Medium/Add_Number_Linked_lists.java
+++ GFG/Medium/Add_Number_Linked_lists.java
@@ -19,3 +19,48 @@ Constraints:
 0 ≤ node->data ≤ 9
 
 */
+class Solution {
+    public Node addTwoLists(Node head1, Node head2) {
+        head1 = reverse(head1);
+        head2 = reverse(head2);
+
+        int carry = 0;
+        Node dummy = new Node(0);
+        Node curr = dummy;
+
+        while (head1 != null || head2 != null || carry != 0) {
+            int sum = carry;
+
+            if (head1 != null) {
+                sum += head1.data;
+                head1 = head1.next;
+            }
+            if (head2 != null) {
+                sum += head2.data;
+                head2 = head2.next;
+            }
+
+            carry = sum / 10;
+            curr.next = new Node(sum % 10);
+            curr = curr.next;
+        }
+
+        Node result = reverse(dummy.next);
+
+        while (result != null && result.data == 0 && result.next != null)
+            result = result.next;
+
+        return result;
+    }
+
+    private Node reverse(Node head) {
+        Node prev = null, curr = head;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
