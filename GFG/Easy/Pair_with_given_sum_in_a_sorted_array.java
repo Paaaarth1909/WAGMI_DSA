76b0edb5200c88fa9cbb1870091e4b661e653aa8
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-11 19:13:07 +0530

    COMMIT

--- GFG/Easy/Pair_with_given_sum_in_a_sorted_array.java
+++ GFG/Easy/Pair_with_given_sum_in_a_sorted_array.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* You are given an integer target and an array arr[]. You have to find number of pairs in arr[] which sums up to target. It is given that the elements of the arr[] are in sorted order.
 Note: pairs should have elements of distinct indexes. 
 
