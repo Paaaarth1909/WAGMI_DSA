4a6a3fe3d39a5079a20908db319b821bff5e8cb3
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-14 21:21:35 +0530

    COMMIT

--- GFG/Easy/Bus_ticket_change.java
+++ GFG/Easy/Bus_ticket_change.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* You are given an array arr[] representing passengers in a queue. Each bus ticket costs 5 coins, and arr[i] denotes the note a passenger uses to pay (which can be 5, 10, or 20). You must serve the passengers in the given order and always provide the correct change so that each passenger effectively pays exactly 5 coins. Your task is to determine whether it is possible to serve all passengers in the queue without ever running out of change.
 
 Examples:
