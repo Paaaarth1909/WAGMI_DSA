be32e8ab6638999e0854e39c4d176f3f5666030b
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-20 23:57:45 +0530

    COMMIT

--- GFG/Easy/Prime_number.java
+++ GFG/Easy/Prime_number.java
@@ -1,3 +1,5 @@
+package GFG.Easy;
+
 /* Given a number n, determine whether it is a prime number or not.
 Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
 
@@ -16,3 +18,14 @@ Constraints:
 1 ≤ n ≤ 109
 
 */
+class Solution {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+
+        return true;
+    }
+}
