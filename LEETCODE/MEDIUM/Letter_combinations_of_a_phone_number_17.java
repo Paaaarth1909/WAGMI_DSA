120c5e89cd65771c79583238586938c3afd9ffbd
Parthsaarthie sharma <toshit113@gmail.com>
2026-01-25 23:43:46 +0530

    COMMIT

--- LEETCODE/MEDIUM/Letter_combinations_of_a_phone_number_17.java
+++ LEETCODE/MEDIUM/Letter_combinations_of_a_phone_number_17.java
@@ -20,3 +20,34 @@ Constraints:
 1 <= digits.length <= 4
 digits[i] is a digit in the range ['2', '9'].
 */
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> res = new ArrayList<>();
+        if (digits.length() == 0) return res;
+
+        String[] map = {
+            "", "", "abc", "def", "ghi", "jkl",
+            "mno", "pqrs", "tuv", "wxyz"
+        };
+
+        dfs(0, digits, map, new StringBuilder(), res);
+        return res;
+    }
+
+    void dfs(int i, String d, String[] map, StringBuilder cur, List<String> res) {
+        if (i == d.length()) {
+            res.add(cur.toString());
+            return;
+        }
+        String letters = map[d.charAt(i) - '0'];
+        for (int j = 0; j < letters.length(); j++) {
+            cur.append(letters.charAt(j));
+            dfs(i + 1, d, map, cur, res);
+            cur.deleteCharAt(cur.length() - 1);
+        }
+    }
+}
