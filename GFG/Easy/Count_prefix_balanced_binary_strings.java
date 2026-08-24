1e7b6576f2f3106bf81b8e9613d32f62c472becf
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-24 20:13:29 +0530

    Commit

--- GFG/Easy/Count_prefix_balanced_binary_strings.java
+++ GFG/Easy/Count_prefix_balanced_binary_strings.java
@@ -1,3 +1,4 @@
+package GFG.Easy;
 /* Given an integer n, count the number of binary strings of length 2 * n that contain exactly n ones and n zeros such that every prefix of the string contains at least as many ones as zeros. Since the answer can be very large, return it modulo 109 + 7.
 
 Examples:
@@ -12,3 +13,44 @@ Constraints:
 
 1 ≤ n ≤ 1000
  */
+class Solution {
+    static final long MOD = 1000000007;
+
+    public int prefixStrings(int n) {
+
+        long[] fact = new long[2 * n + 1];
+        fact[0] = 1;
+
+        for (int i = 1; i <= 2 * n; i++) {
+            fact[i] = (fact[i - 1] * i) % MOD;
+        }
+
+        long numerator = fact[2 * n];
+
+        long denominator = (fact[n] * fact[n]) % MOD;
+
+        // C(n) = (2n)! / (n! * n! * (n+1))
+        denominator = (denominator * (n + 1)) % MOD;
+
+        long inverse = power(denominator, MOD - 2);
+
+        return (int) ((numerator * inverse) % MOD);
+    }
+
+    private long power(long a, long b) {
+
+        long result = 1;
+
+        while (b > 0) {
+
+            if ((b & 1) == 1) {
+                result = (result * a) % MOD;
+            }
+
+            a = (a * a) % MOD;
+            b >>= 1;
+        }
+
+        return result;
+    }
+}
