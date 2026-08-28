3f0a3e7f806e4ce0d3a65eb847d318b2d257e2ed
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-28 20:07:00 +0530

    Commit

--- GFG/Medium/Minimum_cost_selection.java
+++ GFG/Medium/Minimum_cost_selection.java
@@ -1,4 +1,3 @@
-package GFG.Medium;
 /* Given an n × 3 matrix mat[][], where each row represents the costs of three available choices at a shop, select exactly one choice from each row such that the same choice is not selected in two adjacent rows.
 
 Return the minimum total cost required.
@@ -20,3 +19,29 @@ Constraints:
 1 ≤ mat[i][j] ≤ 100
 mat.rows = n
 */
+class Solution {
+    public int minCost(int[][] mat) {
+
+        int n = mat.length;
+
+        int[] dp = new int[3];
+
+        // First row
+        dp[0] = mat[0][0];
+        dp[1] = mat[0][1];
+        dp[2] = mat[0][2];
+
+        for (int i = 1; i < n; i++) {
+
+            int[] next = new int[3];
+
+            next[0] = mat[i][0] + Math.min(dp[1], dp[2]);
+            next[1] = mat[i][1] + Math.min(dp[0], dp[2]);
+            next[2] = mat[i][2] + Math.min(dp[0], dp[1]);
+
+            dp = next;
+        }
+
+        return Math.min(dp[0], Math.min(dp[1], dp[2]));
+    }
+}
