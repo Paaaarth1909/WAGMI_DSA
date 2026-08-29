905c097974d09111a45a9c9de2007fb92e6be14d
Parthsaarthie sharma <toshit113@gmail.com>
2026-08-29 20:36:52 +0530

    Commit

--- GFG/Medium/Count_subsequences_divisble_by_N.java
+++ GFG/Medium/Count_subsequences_divisble_by_N.java
@@ -1,3 +1,4 @@
+package GFG.Medium;
 /* Given a numeric string s containing only digits and an integer n, count the number of non-empty subsequences of s whose numeric value is divisible by n. Return the answer modulo 1e9 + 7.
 
 Examples:
@@ -11,3 +12,41 @@ Explanation: The subsequences 30, 30, 330 and 0 are divisible by 6.
 Constraints:
 1 ≤ |s| * n ≤ 106
 */
+class Solution {
+    public int countSubsequences(String s, int n) {
+
+        final int MOD = 1000000007;
+
+        // dp[r] = number of non-empty subsequences
+        // whose value % n == r
+        long[] dp = new long[n];
+
+        for (int i = 0; i < s.length(); i++) {
+
+            int digit = s.charAt(i) - '0';
+
+            // We must use a separate array so that the current
+            // digit is not used multiple times.
+            long[] next = dp.clone();
+
+            // Subsequence consisting only of this digit
+            next[digit % n]++;
+
+            // Append current digit to every existing subsequence
+            for (int r = 0; r < n; r++) {
+
+                if (dp[r] == 0) {
+                    continue;
+                }
+
+                int newR = (r * 10 + digit) % n;
+
+                next[newR] = (next[newR] + dp[r]) % MOD;
+            }
+
+            dp = next;
+        }
+
+        return (int) (dp[0] % MOD);
+    }
+}
